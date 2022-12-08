29c9e55a2e0be5eea3336c8652b2cc60de897923
Moussa-Kalam <m.amzat@alustudent.com>
2022-12-08 20:47:13 +0200

    Primitive built-in types

---
+++ exercise_files/Ch02/primitive-builtin-types/src/demo.ts
@@ -0,0 +1,12 @@
+let x: number;
+let y: string;
+let z: boolean;
+let a: Date;
+let b: string[];
+let c: number[];
+
+x = 24;
+y = "Moussa";
+z = true;
+a = new Date();
+b = ["Student", "Player", "Greatness"];
